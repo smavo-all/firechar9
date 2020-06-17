3d7542a8e33d17926ff579023e0bc874b2a89a02
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-16 22:06:15 -0500

    add components/chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule} from '@angular/forms';
+
+
 //Importando de Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
+// Add Components
+import { ChatComponent } from './components/chat/chat.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
