006c5d4cbf806e4a51a2a2118d943f6d6f63f8c2
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-17 12:18:10 -0500

    App Finalizada V1.0.0

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -75,9 +75,10 @@ export class ChatService {
   agregarMensaje(texto: string) {
     // TODO falta el UID del usuario
     let mensaje: Mensaje = {
-      nombre: 'demo',
+      nombre: this.usuario.nombre,
       mensaje: texto,
       fecha: new Date().getTime(),
+      uid: this.usuario.uid
     };
     return this.itemsCollection.add(mensaje);
   }
